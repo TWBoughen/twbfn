9cae0fa3e1e30cacb35bdd57494b967494a1ee29
Thomas Boughen <b9031732@ncl.ac.uk>
2024-10-02 01:04:11 +0100

    Fixing errors

--- R/dists.R
+++ R/dists.R
@@ -1,5 +1,14 @@
 #' @export
 dppa = Vectorize(function(x, n0,a,b,eps,lambda,log=T){
+  g = Vectorize(function(x,n0=1,a=1,b=1,eps=0){
+    #nom
+    # const = n0^(1-a)
+    if(x<n0){
+      return(x^a + eps)
+    }else{
+      return(max(0,n0)^a + b*(x-(max(0,n0))) + eps)
+    }
+  }, vectorize.args = 'x')
   if(x==1){
     return(
       -log(1+g(x,n0,a,b,eps)/lambda)
@@ -11,5 +20,14 @@ dppa = Vectorize(function(x, n0,a,b,eps,lambda,log=T){
 }, vectorize.args = 'x')
 #' @export
 pppa = Vectorize(function(x, n0,a,b,eps,lambda,log=T){
+  g = Vectorize(function(x,n0=1,a=1,b=1,eps=0){
+    #nom
+    # const = n0^(1-a)
+    if(x<n0){
+      return(x^a + eps)
+    }else{
+      return(max(0,n0)^a + b*(x-(max(0,n0))) + eps)
+    }
+  }, vectorize.args = 'x')
   return(-sum(log(1+lambda/g(1:x,n0,a,b,eps))))
 }, vectorize.args = 'x')
--- R/lambda_funcs.R
+++ R/lambda_funcs.R
@@ -1,4 +1,13 @@
 lambda_obj = Vectorize(function(l, n0,a,b,eps, mx=1e3){
+  g = Vectorize(function(x,n0=1,a=1,b=1,eps=0){
+    #nom
+    # const = n0^(1-a)
+    if(x<n0){
+      return(x^a + eps)
+    }else{
+      return(max(0,n0)^a + b*(x-(max(0,n0))) + eps)
+    }
+  }, vectorize.args = 'x')
   n = 1:mx
   lgs  = log(1 + l/g(n,n0,a,b,eps))
   return(sum(exp(-cumsum(lgs))))
